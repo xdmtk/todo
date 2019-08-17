7c0cce3043b3fefef176a5482802a15a5d984c6b
Nick Martinez <nick@xdmtk.org>
2019-08-17 11:06:58 -0700

    Build priority class

--- include/todo/priority.h
+++ include/todo/priority.h
@@ -1,13 +1,31 @@
 #pragma once
+#include <iostream>
+
+/**
+ * This class will manage the data structure of elements in the todolist.
+ * A TodoList will map out several Priority objects, with each priority object
+ * containing:
+ *
+ *      Priority Name
+ *      Priority Level [ 0, inf )
+ *      Priority Color ( For terminal output )
+ *      Todo strings
+ *
+ */
 
 class Priority {
-
     public:
-        Priority();
+        Priority(std::string name, int pri_level, Color color, std::string description);
+        Priority(std::string data);
 
+        enum Color {RED, GREEN, YELLOW, ORANGE, BLUE};
+        std::string get_raw();
 
-
-
-
+    private:
+        std::string name;
+        int pri_level;
+        Color color;
+        std::string description;
+        std::string raw;
 
 };
--- src/priority.cpp
+++ src/priority.cpp
@@ -1,9 +1,27 @@
 #include <todo/priority.h>
 #include <iostream>
 
-Priority::Priority() {
+
+// This constructor can take raw data from the config and build the request priority object
+Priority::Priority(std::string data) {
     
-    std::cout << "New Priority class";
+
+
+}
+
+// This constructor is meant to be called when internal usage wants to define a Priority object
+// to insert into the config. It will formulate the raw string data
+Priority::Priority(std::string name, int pri_level, Color color, std::string description) {
+
+
+
+
+}
+
+
+std::string Priority::get_raw() {
+
+
 
 
 }
