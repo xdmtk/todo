1b4ef07853cad95ce7d3da25240fd3c09d125ccf
Nick Martinez <nick@xdmtk.org>
2019-08-18 14:43:09 -0700

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,19 @@
 
 int main(int argc, char **argv) {
 
+    // Collect and parse arguments
     auto a = Arguments(argv, argc);
+
+    // Parse TodoList data from config
     auto tl = TodoList(Config::check_config());
+
+    // Setup Printer object to display TodoList data
     auto p = Printer(&tl);
+
+    // Setup Caller object to direct control flow based
+    // on given arguments
     auto c = Caller(&a, &p, &tl);
 
     c.call_function();
-
 }
 
