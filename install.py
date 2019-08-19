25b6f959e8f27a123361e53d40fc14dbb2d547c8
Nick Martinez <nick@xdmtk.org>
2019-08-19 11:34:07 -0700

    Eliminate need for sudo

--- install.py
+++ install.py
@@ -1,8 +1,5 @@
 import subprocess, os, sys, shutil
 
-if os.geteuid() != 0:
-    print("To install, please run this script as root!")
-    sys.exit(0)
 
 bin_deps = [
     'cmake',
@@ -25,7 +22,9 @@ if not os.path.isfile("CMakeLists.txt"):
 print("[+] Generating Makefile with CMake")
 os.mkdir("build")
 os.chdir("build")
-subprocess.call(['cmake', '..'])
+ret = subprocess.call(['cmake', '..'])
+if ret != 0:
+    print("[-] CMake build failed, check corresponding error message")
 if os.path.isfile("/home/" + os.getenv("USER") + "/bin/todo"):
     print("[-] Todo executable already exists in local bin folder. Halting installation..")
     sys.exit(0)
@@ -41,11 +40,12 @@ subprocess.call(['cp', 'todo', '/home/' + os.getenv("USER") + '/bin'])
 if not os.path.isdir("/etc/bash_completion.d"):
     print("[-] Bash completion script directory does not exist, creating..."
           " You will probably need to source this directory in your bashrc to enable auto-completion features")
+    subprocess.call(['sudo', 'mkdir', '/etc/bash_completion.d'])
     os.mkdir("/etc/bash_completion.d")
 
 os.chdir("..")
 print("[+] Copying bash completion script..")
-subprocess.call(['cp', 'scripts/todo_completion.sh', '/etc/bash_completion.d'])
+subprocess.call(['sudo', 'cp', 'scripts/todo_completion.sh', '/etc/bash_completion.d'])
 
 print("[+] Done")
 
